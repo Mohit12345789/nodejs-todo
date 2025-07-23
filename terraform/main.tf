9ba27dc02cf4766270eedf9ec0710a8b9018185c
Mohit Verma <156345473+Mohit12345789@users.noreply.github.com>
2025-07-23 14:44:47 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,11 @@
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.0"
-
-  name = "eks-vpc"
-  cidr = var.vpc_cidr
-
-  azs = ["ap-south-1a", "ap-south-1b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = { Name = "eks-vpc" }
+  source = "terraform-aws-modules/vpc/aws"
+  ...
+  enable_classiclink = var.enable_classiclink
+  enable_classiclink_dns_support = var.enable_classiclink_dns_support
+  ...
 }
+
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
